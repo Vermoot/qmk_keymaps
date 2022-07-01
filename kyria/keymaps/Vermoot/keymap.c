63e9f5ab947d35a3de6448e099e195bc3a6a52e2
Vermoot <vermoot@gmail.com>
2022-07-01 18:27:09 +0200

    kyria

--- kyria/keymaps/Vermoot/keymap.c
+++ kyria/keymaps/Vermoot/keymap.c
@@ -9,7 +9,8 @@ enum layers {
     _MEDIA,
     _FUNCT,
     _SELECT,
-    _UBERBASE
+    _UBERBASE,
+    _GAMING
 };
 
 #include "custom_functions.c"
@@ -31,6 +32,28 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+  switch (keycode) {
+    case KC_A ... KC_Z:
+    case KC_MINS:
+    case E_ACUTE ... C_CED:
+      add_weak_mods(MOD_BIT(KC_LSFT));
+      return true;
+
+    case KC_1 ... KC_0:
+    case KC_BSPC:
+    case KC_DEL:
+    case KC_UNDS:
+    case KC_SPC:
+    case UD_APO ... UD_CIRC:
+    case KC_QUOT:
+      return true;
+
+    default:
+      return false;
+  }
+}
+
 // ---- Combo terms ---- {{{
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
   switch (index) {
@@ -39,7 +62,8 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
     /* case OU_OU: */
     /* case AR_A_GRV: */
     /* case NI_HRM_BSPC: */
-      /* return COMBO_TERM - 10; */
+    case DEAD_UMLAUT:
+      return COMBO_TERM - 10;
 
     // Longer
     case BOU_BEAUCOUP:
@@ -49,7 +73,6 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
     case AO_OS_SHIFT:
     case CM_COMME:
     case PLOVER_RET:
-    case DEAD_UMLAUT:
       return COMBO_TERM + 20;
 
     // Long
@@ -124,6 +147,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 // ---- END STENO ---- }}}
 
+// ---- GAMING ---- {{{
+[_GAMING] = LAYOUT(
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_M,    KC_SCLN,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,       _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                             _______, NUM,     NAV,     KC_SPC,  MO(_UBERBASE), LOWER,   KC_SPC,  KC_BSPC, _______, DF(_BASE)
+),
+// ---- END GAMING ---- }}}
+
 // ---- NAV ---- {{{
 [_NAV] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                                           _______, KC_PGDN, KC_PGUP, _______, _______, _______,
@@ -171,10 +203,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // ---- SELECT ---- {{{
 [_SELECT] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, STENO,
-  _______, _______, _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+  _______, DF(_GAMING),_______, _______, _______, _______,                                           _______, _______, _______, _______, _______, STENO,
+  _______, _______,    _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, _______,
+  _______, _______,    _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______,
+                                _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 // ---- END SELECT ---- }}}
 
