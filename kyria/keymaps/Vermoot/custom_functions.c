dc1704e46a7eff958ab3a64a367cba8d23fe530a
Vermoot <vermoot@gmail.com>
2022-08-03 18:14:41 +0200

    deal with weak mods in an attempt to make caps_word work correctly with accented letters

--- kyria/keymaps/Vermoot/custom_functions.c
+++ kyria/keymaps/Vermoot/custom_functions.c
@@ -11,12 +11,15 @@ bool undead(uint16_t  character, bool pressed) {
 void unmod(uint16_t keycode) {
   uint8_t mod_state = get_mods();
   uint8_t osm_state = get_oneshot_mods();
+  uint8_t weak_mods_state = get_weak_mods();
   /* clear_mods(); clear_oneshot_mods(); clear_macro_mods(); clear_weak_mods(); */
   unregister_mods(MOD_MASK_SHIFT);
   del_oneshot_mods(MOD_MASK_SHIFT);
+  del_weak_mods(MOD_MASK_SHIFT);
   tap_code16(keycode);
   register_mods(mod_state);
   add_oneshot_mods(osm_state);
+  add_weak_mods(weak_mods_state);
 }
 
 bool accented_letter(uint16_t accent, uint16_t letter, bool pressed) {
